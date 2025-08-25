d1ab9b6e338a7467a6bb4f954a9cf97c34d84c7b
Max Erfodimo <maxerfodimo@gmail.com>
2025-08-25 20:47:51 +0200

    add google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T9FFFPPG');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZYMWHJ9XWQ"></script>
         <script
@@ -40,6 +54,17 @@ export default function Document() {
         {/* PASTE YOUR COOKIEHUB CMP CODE SNIPPET HERE */}
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9FFFPPG"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Main />
         <NextScript />
       </body>
