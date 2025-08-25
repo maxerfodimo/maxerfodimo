df670fc3b84812952a241beb9f09d8fe159e814a
Max Erfodimo <maxerfodimo@gmail.com>
2025-08-25 20:46:59 +0200

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,27 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
+              // Define dataLayer and the gtag function.
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
+              
+              // Set default consent for specific regions according to your requirements
+              gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied',
+                'regions':[]
+              });
+              
+              // Set default consent for all other regions according to your requirements
+              gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied'
+              });
+              
               gtag('js', new Date());
               gtag('config', 'G-ZYMWHJ9XWQ');
             `,
